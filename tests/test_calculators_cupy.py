80d5efafb95e08e944534f341f612229b5d18ec5
josephwkania <60994292+josephwkania@users.noreply.github.com>
2021-09-02 18:41:19 -0400

    added skip to calculators_cupy.py

--- tests/test_calculators_cupy.py
+++ tests/test_calculators_cupy.py
@@ -2,13 +2,23 @@
 """
 Tests for calculator_cupy.py
 """
+try:
+    import cupy as cp
 
-import cupy as cp
+    CUPY_AVAL = True
+except ModuleNotFoundError:
+    CUPY_AVAL = False
+
+import pytest
 
 import jess.calculators_cupy as calc
 
 
+@pytest.mark.skipif(not CUPY_AVAL, reason="cupy not available.")
 def test_to_dtype():
+    """
+    Create some random data and turn it into uint8
+    """
     random = cp.random.normal(scale=12, size=512 * 512).reshape(512, 512)
     random_8 = cp.around(random)
     random_8 = cp.clip(random_8, 0, 255)
