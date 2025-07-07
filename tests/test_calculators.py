51a218c28d7b1e8efff8619011892fb87ede6393
josephwkania <60994292+josephwkania@users.noreply.github.com>
2025-07-07 02:49:27 +0100

    Fix: Add padding test to tests.test_calculators.TestAccumulate.

--- tests/test_calculators.py
+++ tests/test_calculators.py
@@ -60,6 +60,29 @@ class TestAccumulate:
             ),
         )
 
+    def test_pad(self):
+        """
+        Test accumulate on axis=1 with padding using the edge value.
+        """
+        padded_to_accumulate = np.pad(self.to_accumulate, ((0, 1), (0, 0)), mode="edge")
+        accumulates_hor = calc.accumulate(
+            padded_to_accumulate, factor=2, axis=1, pad="edge"
+        )
+        assert np.array_equal(
+            accumulates_hor,
+            np.asarray(
+                [
+                    3 * [2 * 0],
+                    3 * [2 * 1],
+                    3 * [2 * 2],
+                    3 * [2 * 3],
+                    3 * [2 * 5],
+                    3 * [2 * 6],
+                    3 * [2 * 6],
+                ]
+            ),
+        )
+
     def test_not_implemented(self):
         """
         Raise error for higher axis
